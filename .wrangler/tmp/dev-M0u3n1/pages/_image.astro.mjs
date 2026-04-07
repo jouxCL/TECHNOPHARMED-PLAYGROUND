36ef38b06f5c7ded857f61e2aabdd586336280cd
jouxCL <juansi1582@gmail.com>
2026-04-06 19:52:33 -0500

    Feat: Paginas en desarrollo

---
+++ .wrangler/tmp/dev-M0u3n1/pages/_image.astro.mjs
@@ -0,0 +1,3 @@
+globalThis.process ??= {}; globalThis.process.env ??= {};
+export { a as page } from '../chunks/image-endpoint_rpnX2R9j.mjs';
+export { renderers } from '../renderers.mjs';
---
+++ .wrangler/tmp/pages-PjbCnM/pages/llenadoras.astro.mjs
@@ -0,0 +1,23 @@
+globalThis.process ??= {}; globalThis.process.env ??= {};
+import { e as createComponent, k as renderComponent, r as renderTemplate, m as maybeRenderHead } from '../chunks/astro/server_CrBcLEcb.mjs';
+import { $ as $$Layout } from '../chunks/Layout_CkICJ7Lj.mjs';
+/* empty css                                      */
+export { renderers } from '../renderers.mjs';
+
+const $$Llenadoras = createComponent(($$result, $$props, $$slots) => {
+  return renderTemplate`${renderComponent($$result, "Layout", $$Layout, { "title": "Llenadoras de L\xEDquidos \u2013 Technopharmed", "description": "FILAMATIC \u2013 automatizaci\xF3n de llenado de l\xEDquidos para la industria farmac\xE9utica." }, { "default": ($$result2) => renderTemplate` ${maybeRenderHead()}<div class="development-page"> <div class="development-content"> <img src="/logos/logo1.svg" alt="Technopharmed Logo" class="development-logo"> <p class="development-title">Página en construcción</p> </div> </div> ` })} `;
+}, "C:/Users/juans/Downloads/Micelaneas/Legion Web/Repositorios/Technopharmed playground/src/pages/llenadoras.astro", void 0);
+
+const $$file = "C:/Users/juans/Downloads/Micelaneas/Legion Web/Repositorios/Technopharmed playground/src/pages/llenadoras.astro";
+const $$url = "/llenadoras";
+
+const _page = /*#__PURE__*/Object.freeze(/*#__PURE__*/Object.defineProperty({
+  __proto__: null,
+  default: $$Llenadoras,
+  file: $$file,
+  url: $$url
+}, Symbol.toStringTag, { value: 'Module' }));
+
+const page = () => _page;
+
+export { page };
